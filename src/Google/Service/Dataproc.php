0a7ef99452843c2eb0bed3520ec384b7d378b0cd
Google APIs <googleapis-publisher@google.com>
2017-06-19 08:48:38 +0000

    Autogenerated update for dataproc version v1 (2017-06-19)

--- src/Google/Service/Dataproc.php
+++ src/Google/Service/Dataproc.php
@@ -376,10 +376,6 @@ class Google_Service_Dataproc extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'pageSize' => array(
-                  'location' => 'query',
-                  'type' => 'integer',
-                ),
                 'filter' => array(
                   'location' => 'query',
                   'type' => 'string',
@@ -387,6 +383,10 @@ class Google_Service_Dataproc extends Google_Service
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
+                ),
+                'pageSize' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
                 ),
               ),
             ),
