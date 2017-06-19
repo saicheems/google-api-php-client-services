c1dee170ffb1dbd3eb958bda4b2d31b9d2d0a2f8
Google APIs <googleapis-publisher@google.com>
2017-06-19 08:51:13 +0000

    Autogenerated update for logging version v2 (2017-06-19)

--- src/Google/Service/Logging.php
+++ src/Google/Service/Logging.php
@@ -327,13 +327,13 @@ class Google_Service_Logging extends Google_Service
               'path' => 'v2/monitoredResourceDescriptors',
               'httpMethod' => 'GET',
               'parameters' => array(
-                'pageToken' => array(
-                  'location' => 'query',
-                  'type' => 'string',
-                ),
                 'pageSize' => array(
                   'location' => 'query',
                   'type' => 'integer',
+                ),
+                'pageToken' => array(
+                  'location' => 'query',
+                  'type' => 'string',
                 ),
               ),
             ),
@@ -537,13 +537,13 @@ class Google_Service_Logging extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
-                'pageSize' => array(
-                  'location' => 'query',
-                  'type' => 'integer',
-                ),
                 'pageToken' => array(
                   'location' => 'query',
                   'type' => 'string',
+                ),
+                'pageSize' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
                 ),
               ),
             ),'update' => array(
